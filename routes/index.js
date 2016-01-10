042beebaea3e581718eef003cb65fa6f06bf74fb
Sanket <sanket.phansekar@outlook.com>
2016-01-10 18:44:04 +0530

    Code Improvisation as per standards

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Activity = require('../models/Activity');
+var User = mongoose.model('User');
 var passport = require('passport');
-// var Activity = mongoose.model('Activity');
+var Activity = mongoose.model('Activity');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Angular App' });
 });
 
 router.get('/activity', function(req, res) {
@@ -48,19 +48,49 @@ router.get('/login',function(req,res){
 router.get('/signup' ,function(req,res){
 		res.render('signup');
 	});
-router.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/signup' // redirect back to the signup page if there is an error
-     
-    }));
+router.post('/signup', function (req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  User.findOne({'email': email}, function (err,user) {      
+      if(err) {
+        res.json({error : 'Internal error. Please try again'});
+        return;
+      }
 
-router.post('/login',passport.authenticate('local-login',{
-      	successRedirect : '/profile',
-      	failureRedirect : '/login'
-      }));
+      if(user){
+        res.json({error : 'User already exists.'});
+        return;
+      }else{
+        var newUser = new User(); 
 
-    
+        newUser.email = email;
+        newUser.password = newUser.generateHash(password); //genearting salt password
 
+      //save the new user
+      newUser.save(function (err) {
+        if(err) {
+          res.json({error : 'Error in creation of user'});
+        } else {
+          res.json({success : 'User created successfully.'});
+        }
+      }); 
+      
+    }
+  });
+});
 
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', function(err, user, info){
+    if(err){ 
+      return next(err); 
+    }
 
+    if(user){
+      return res.json({success : user });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+   
 module.exports = router;
