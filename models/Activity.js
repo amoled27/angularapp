5dbda270e659488d6d2c2768d5474c4906f9b344
Sanket <sanket.phansekar@outlook.com>
2016-01-10 18:42:15 +0530

    Registered schema to model

--- models/Activity.js
+++ models/Activity.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var activitySchema = new mongoose.Schema({
 	name: String,
 	status: Boolean
-})
+});
 
 mongoose.model('Activity',activitySchema);
-module.exports.Activity = mongoose.model('Activity');
